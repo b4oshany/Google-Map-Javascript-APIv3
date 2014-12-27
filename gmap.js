6c09810b2e89715d056dc1ea2d6a9b9323101538
Oshane Bailey <b4.oshany@gmail.com>
2014-12-27 09:17:06 -0500

    Added search by text.

--- gmap.js
+++ gmap.js
@@ -10,25 +10,79 @@ function GMaps(map_canvas, main_cords){
     this.markers = {};
     this.placeService = null;
     this.map_canvas = map_canvas;
+    /**
+    * @property {object} settings                           - GMaps object settings.
+    * @property {boolean} showPlacesNearby                  - Set to true if places nearby should be shown near
+    *   the default or centered location.
+    * @property {object} location                           - The default location of the map. This is also used by
+    *   the queries.  
+    */
+    this.settings = {
+        showPlacesNearby: false,
+        location: {lat: 18.0030, lng: -76.7446},
+        limitResults: 10
+    }
+    /**
+    * @property {int} zoom                                  - Zoom Level of the map, which ranges from 1-22.
+    *   This can be invoke throw the map.setZooom method.
+    * @property {object} center                             - Set the given location as the center of the map.
+    *   This can be invoke through the map.setCenter function. 
+    */
     this.mapOptions = {
         zoom: 12,
-        center:  {lat: 18.0030, lng: -76.7446},
+        center:  this.settings.location,
     };
-    this.nearbyPlacesQueryFor = {
-        location: this.mapOptions.center,
-        radius: 150,
-        types: [],
+    /**
+    * @property {object} queryBy                            - Type of queries that can be done.
+    * @property {object} queryBy.map                        - The map to be queried.
+    * @property {object} queryBy.nearbyPlaces               - Query places by nearby palces.
+    * @property {object} queryBy.nearbyPlaces.location      - Location of object.
+    * @property {int} queryBy.nearbyPlaces.radius           - Radius of the location to be query.
+    * @property {string[]} queryBy.nearbyPlaces.types       - List of types of places to be return in the query result.
+    * @property {object} queryBy.text                       - Query places by string.
+    * @property {string} queryBy.text.query                 - Query text to search for places.
+    * @proeprty {boolean} queryBy.text.openNow              - Set to true if only business or places that are currently open
+    *   should be include in the result.
+    * @property {object} queryBy.text.bounds                - (google.maps.LatLngBounds) Boundary of the places to be query.
+    * @property {object} queryBy.text.location              - (google.maps.LatLng) Location of the place to be query.
+    * @property {string[]} queryBy.text.types               - List of types of places to be return in the query result.
+    * @property {int} queryBy.text.radius                   - Radius of the location to be query.
+    */
+    this.queryBy = {
+        map: this,
+        updateLocation: function(location){
+            this.nearbyPlaces.location = location;
+        },
+        nearbyPlaces: {
+            location: this.mapOptions.center,
+            radius: 150,
+            types: [],
+        },
+        text: {
+            query: '',
+            openNow: false,
+            types: [],
+        }
     };
     if(main_cords){
         this.mapOptions.center = main_cords;
-        this.nearbyPlacesQueryFor.location = main_cords;
+        this.queryBy.updateLocation(main_cords);
     }
+    /**
+    * Initiate google maps.
+    */
     this.initialize = function() {
+        GMaps.activeMap = this;
         this.map = new google.maps.Map(document.getElementById(this.map_canvas),
             this.mapOptions);
         GMaps.isAvailable = true;
     };
+    /**
+    * Create a marker and add it to the map.
+    * @return google.maps.Marker
+    */
     this.createMarker = function(content, place, options){
+        GMaps.activeMap = this;
         var map = this.map
         var marker = new google.maps.Marker({
             map: map,
@@ -43,31 +97,64 @@ function GMaps(map_canvas, main_cords){
             infoWindow.open(map, this);
         });
         this.markers[place.placeId] = marker;
+        if(this.settings.showPlacesNearby){
+            this.nearbyPlacesSearch(place);
+            this.map.setCenter(place.location);
+        }
         return marker;
     };
+    /**
+    * Create a place service.
+    * @return google.maps.palces.PlacesService
+    */
     this.initiateSearchService = function(){
+        GMaps.activeMap = this;
         return new google.maps.places.PlacesService(this.map);
     };
-    this.nearbyPlacesSearch = function(place, callback){
+    /**
+    * Perform a nearby search.
+    */
+    this.nearbyPlacesSearch = function(place, callback, options){
         var service = this.initiateSearchService();
         if(callback == undefined)
-            callback = this.mapNearbyPlaces;
-        var nearbyOptions = {
-            location: place.location,
-            radius: this.nearbyPlacesQueryFor.radius
-        }
-        service.nearbySearch(nearbyOptions, callback)
+            callback = this.mapQueryResult;
+        options = this.queryBy.nearbyPlaces;
+        options.location = place.location;
+        service.nearbySearch(options, callback)
     };
-    this.mapNearbyPlaces = function(results, status){
+    /**
+    * Perform the text search.
+    * @param {string} searchText                        - Text to query by.
+    * @callback {resultCallback} callback               - Callback function to render
+    *   the query result. By default it uses GMaps.mapQueryResult function.
+    * @param {object} options                           - Additional query options.
+    */
+    this.textSearch = function(searchText, callback, options){
+        var service = this.initiateSearchService();
+        if(callback == undefined)
+            callback = this.mapQueryResult;
+        options = this.queryBy.text;
+        options.query = searchText;
+        service.textSearch(options, callback);
+    }
+    /**
+    * Add the query result to the map.
+    */
+    this.mapQueryResult = function(results, status){
         if (status == google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
+            for (var i = 0; i < results.length && i < GMaps.activeMap.settings.limitResults; i++) {
                 var place = results[i];
-                GMaps.activeObject.createMarker(GMaps.nearbyPlacesResultFormat(place),
+                GMaps.activeMap.createMarker(GMaps.formatQueryResult(place),
                     GMaps.createPlaceByLocation(place.place_id, place.geometry.location));
             }
         }
     };
-    this.requestPlaceInfo = function(place, placesNearbyTitle, infoWindowContent){
+    /**
+    * Request information of a location.
+    * @param {object} place Location object.
+    * @param {object} InfoWindow to add the place information.
+    */
+    this.requestPlaceInfo = function(place, infoWindowContent){
         // Initiate a place request object.
         var service = this.initiateSearchService();
         gmap = this;
@@ -81,12 +168,17 @@ function GMaps(map_canvas, main_cords){
                 location: placeResult.geometry.location
             }
             gmap.createMarker(content, placeGeo);
-            GMaps.activeObject = gmap;
-            gmap.nearbyPlacesSearch(place);
             gmap.map.setZoom(18);
             gmap.map.setCenter(place.location);
+            if(gmap.settings.showPlacesNearby){
+                gmap.nearbyPlacesSearch(place);
+                gmap.map.setCenter(place.location);
+            }
         });
     };
+    /**
+    * Place marker on the map and center it.
+    */
     this.placeMarker = function(location) {
       var marker = new google.maps.Marker({
           position: location,
@@ -94,12 +186,24 @@ function GMaps(map_canvas, main_cords){
       });
       this.map.setCenter(location);
     };
+    /**
+    * Add Marker when the map is clicked.
+    */
     this.clickToAddMaker = function(){
         var gmap = this;
         google.maps.event.addListener(this.map, 'click', function(event) {
             gmap.placeMarker(event.latLng);
         });
     };
+    /**
+    * Clear map of all markers.
+    */
+    this.clearMap = function(){
+        for(x in this.markers){
+            this.markers[x].setMap(null);
+        }
+        this.markers = [];
+    }
 }
 
 GMaps.createInfoWindow = function(content){
@@ -123,7 +227,7 @@ GMaps.geometryToCords = function(place){
     var cords = place.geometry.location;
     return GMaps.createCords(cords.k, cords.D);
 }
-GMaps.nearbyPlacesResultFormat = function(place){
+GMaps.formatQueryResult = function(place){
     var img = document.createElement("img");
     img.src = place.icon;
     var name = document.createElement("h3");
@@ -136,8 +240,8 @@ GMaps.nearbyPlacesResultFormat = function(place){
     tags.innerHTML = place.types.toString();
     article.appendChild(img, name, tags);
     return article;
-}
-GMaps.activeObject = null;
+} 
+GMaps.activeMap = null;
 GMaps.isAvaliable = false;
 
 
